e04c7246d75c5ce085d588617895346857e47e37
Welington Silva <welington.manoels@gmail.com>
2020-11-14 21:47:57 -0300

    chore(deps): fixing dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import analyze from 'rollup-plugin-analyzer';
 import babel from '@rollup/plugin-babel';
+import del from 'rollup-plugin-delete';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
@@ -11,38 +12,19 @@ import pkg from './package.json';
 module.exports = [
   {
     input: 'src/index.js',
-    output: {
-      name: 'use-slice',
-      file: pkg.browser,
-      format: 'umd',
-    },
-    plugins: [
-      babel({
-        exclude: 'node_modules/**', // only transpile our source code
-      }),
-      resolve(), // so Rollup can find `ms`
-      commonjs(), // so Rollup can convert `ms` to an ES module
-      analyze({
-        hideDeps: true,
-        summaryOnly: true,
-        filter: module => /^\/src/.test(module.id),
-      }),
-    ],
-    external: ['react'],
-  },
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
-  {
-    input: 'src/index.js',
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
+      {
+        name: 'use-slice',
+        file: pkg.browser,
+        format: 'umd',
+      },
+      [
+        { name: 'use-slice', file: pkg.main, format: 'cjs' },
+        { name: 'use-slice', file: pkg.module, format: 'es' },
+      ],
     ],
     plugins: [
+      del({ targets: [`dist/`] }),
       babel({
         exclude: 'node_modules/**', // only transpile our source code
       }),
